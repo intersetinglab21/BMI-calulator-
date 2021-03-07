eef69cdb8ccceac9498100f2aa66fc12c3bc135f
intersetinglab21 <77334688+intersetinglab21@users.noreply.github.com>
2021-03-07 08:38:13 +0530

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,4 @@
+mass=float(input("enter your bodyweigh mass: "))
+height=float(input("enter your height in cms: "))
+BMI=mass/height*height
+print("your BMI is: ",BMI)
---
+++ README .txt
@@ -0,0 +1,2 @@
+This is A BMI calculator 
+there is a python script 
